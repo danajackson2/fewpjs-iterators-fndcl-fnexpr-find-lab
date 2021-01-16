814a9ee87d2b5e845a15c63ef6eb4566f4bb4c49
danajackson2 <danajackson2@gmail.com>
2021-01-15 19:42:13 -0800

    commit tree 041ea418a2d4813bdf60efb2fb8b412cb12fe438

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let result = array.find(item => item["result"] === "W")
+  return result? result.year : result
+}
